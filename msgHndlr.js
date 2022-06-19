c30102c2e340c3e9bf2d554db9e8dd4af254d334
Pedro Marinho <pedro.marinho.silva05@aluno.ifce.edu.br>
2022-06-19 14:44:33 -0300

    fix ban

--- msgHndlr.js
+++ msgHndlr.js
@@ -640,7 +640,7 @@ const msgHandler = async (client = new Client(), message) => {
 					if (mentionedJidList.includes(ownerNumber[0])) return client.reply(from, 'Sabe algo que não vou fazer? Banir a mim mesmo!', id);
 					if (mentionedJidList.includes(liderNumber[0])) return client.reply(from, 'Sabe algo que não vou fazer? Banir a mim mesmo!', id);
 					console.log("chegou aqui")
-					await client.sendText(from, `Pronto! removido \n${mentionedJidList.map(user => `@${user.replace(/@c.us/g, '')}`).join('\n')}`);
+					await client.sendText(from, `Pronto! removido ${mentionedJidList[0]}`);
 					console.log("nao quebrou")
 					for (let mentioned of mentionedJidList) {
 						console.log(mentioned)
